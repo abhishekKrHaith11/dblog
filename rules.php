22869d7dad0e2dd0ec045bde3baba6d8183988cb
abhishekKrHaith11 <abhishek.haith11@gmail.com>
2018-06-25 10:07:23 +0530

    remove unnecessary files

--- rules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: abhishekkrhaith11, dblog, abhishekkrhaith11, dblog
-return [
-    'src/MyPackage.php' => 'src/dblog.php',
-    'config/mypackage.php' => 'config/dblog.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/dblog.php',
-    'src/MyPackageServiceProvider.php' => 'src/dblogServiceProvider.php',
-];
