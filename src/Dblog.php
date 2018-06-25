67043e3a813db6a4f4dfcbd58b933c47a2ee6283
abhishekKrHaith11 <abhishek.haith11@gmail.com>
2018-06-25 11:11:28 +0530

    Namespace fix
    Fix of Dblog model namespace use in Dblog class

--- src/Dblog.php
+++ src/Dblog.php
@@ -2,56 +2,56 @@
 
 namespace Abhishekkrhaith11\Dblog;
 
-use Abhishekkrhaith11\Dblog\Models\Dblog;
+use Abhishekkrhaith11\Dblog\Models\Dblog as MDblog;
 
-class dblog
+class Dblog
 {
 
     public function emergency($message)
     {
-    	return Dblog::create(['type' => 'emergency', 'message' => $message]);
+    	return MDblog::create(['type' => 'emergency', 'message' => $message]);
     }
 
 
     public function alert($message)
     {
-    	return Dblog::create(['type' => 'alert', 'message' => $message]);
+    	return MDblog::create(['type' => 'alert', 'message' => $message]);
     }
 
 
     public function critical($message)
     {
-    	return Dblog::create(['type' => 'critical', 'message' => $message]);
+    	return MDblog::create(['type' => 'critical', 'message' => $message]);
     }
 
 
     public function error($message)
     {
-    	return Dblog::create(['type' => 'error', 'message' => $message]);
+    	return MDblog::create(['type' => 'error', 'message' => $message]);
     }
 
 
     public function warning($message)
     {
-    	return Dblog::create(['type' => 'warning', 'message' => $message]);
+    	return MDblog::create(['type' => 'warning', 'message' => $message]);
     }
 
 
     public function notice($message)
     {
-        return Dblog::create(['type' => 'notice', 'message' => $message]);
+        return MDblog::create(['type' => 'notice', 'message' => $message]);
     }
 
 
     public function info($message)
     {
-        return Dblog::create(['type' => 'info', 'message' => $message]);
+        return MDblog::create(['type' => 'info', 'message' => $message]);
     }
 
 
 	public function debug($message)
     {
-        return Dblog::create(['type' => 'debug', 'message' => $message]);
+        return MDblog::create(['type' => 'debug', 'message' => $message]);
     }
 
 }
